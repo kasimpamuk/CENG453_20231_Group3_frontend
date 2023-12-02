994bfcc61ccc32cd38351017a3319f0227ff38e3
kasimpamuk <kasim.pamuk@metu.edu.tr>
2023-12-02 20:04:58 +0300

    initialize Spring Boot project

---
+++ src/main/java/io/github/alprKeskin/kasimpamuk/thesettlersofcatan/TheSettlersOfCatanApplication.java
@@ -0,0 +1,13 @@
+package io.github.alprKeskin.kasimpamuk.thesettlersofcatan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSettlersOfCatanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSettlersOfCatanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/alprKeskin/kasimpamuk/thesettlersofcatan/TheSettlersOfCatanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alprKeskin.kasimpamuk.thesettlersofcatan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSettlersOfCatanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
